1929a46ba86025578ba0795024d6910f8d8227c3
unknown <842402550@qq.com>
2019-05-13 22:29:35 +0800

    delete

--- app/src/main/java/com/example/dell/myapplication/CostBean.java
+++
@@ -1,9 +0,0 @@
-package com.example.dell.myapplication;
-
-import java.io.Serializable;
-
-public class CostBean implements Serializable {
-    public  String costDate;
-    public  String costMoney;
-    public  String costTittle;
-}
